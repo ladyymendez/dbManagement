50c5951f572adf2ab387c832216606758c44a5ac
ladyymendez <ladyy.mendez@gmail.com>
2020-06-08 16:15:57 -0500

    Adding connection and routes

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const connection = require('./mongodb');
+const userRoutes = require('./routes/user/user');
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRoutes);
+
+connection().then(() => {
+  console.log('Connection DB');
+});
 
 app.listen(3000, () => {
   console.log('Express server started on port 3000');
